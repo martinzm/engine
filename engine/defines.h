a69469b5afd65191a562f5418fd0866ae73cdade
mrt <mrt@centrum.cz>
2023-01-30 21:57:23 +0100

    added Licensing

--- engine/defines.h
+++ engine/defines.h
@@ -1,9 +1,21 @@
 /*
- *
- * $Id: defines.h,v 1.14.2.11 2007/01/31 19:59:53 mrt Exp $
- *
- */
- 
+    Carrot is a UCI chess playing engine by Martin Žampach.
+    <https://github.com/martinzm/Carrot>     <martinzm@centrum.cz>
+
+    Carrot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Carrot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 #ifndef DEFINES_H
 #define DEFINES_H
 
--- engine/pers.h
+++ engine/pers.h
@@ -1,8 +1,20 @@
 /*
- *
- * $Id: pers.h,v 1.1.2.5 2006/02/09 20:30:07 mrt Exp $
- *
- */
+    Carrot is a UCI chess playing engine by Martin Žampach.
+    <https://github.com/martinzm/Carrot>     <martinzm@centrum.cz>
+
+    Carrot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Carrot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
  
 #ifndef PERS_H
 #define PERS_H
