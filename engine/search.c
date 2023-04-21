0e6ca282475d60e269059b357e2f95eff7395c1b
mrt <mrt@centrum.cz>
2023-04-21 23:03:44 +0200

    move difficulty adjustments

--- engine/search.c
+++ engine/search.c
@@ -1398,8 +1398,8 @@ int ABNew(board *b, int alfa, int beta, int depth, int ply, int side, tree_store
 	return mb->real_score;
 }
 
-#define MISn 600
-#define MISc 100
+#define MISn 575
+#define MISc 120
 
 int IterativeSearchN(board *b, int alfa, int beta, int depth, int side, int start_depth, tree_store *tree)
 {
@@ -1691,13 +1691,15 @@ int IterativeSearchN(board *b, int alfa, int beta, int depth, int side, int star
 							break;
 						} else {
 							changes++;
-//							if(((changes >= 2) || (tree->tree[ply][ply].move!=b->p_pv.line[0].move)) 
-//							&& (f > (start_depth + 1))) {
-							if(((changes >= 2)) 
+							if(((changes >= 2) || (tree->tree[ply][ply].move!=b->p_pv.line[0].move)) 
 							&& (f > (start_depth + 1))) {
+//							if(((changes >= 2)) 
+//							&& (f > (start_depth + 1))) {
 //								b->search_dif = Min(1000, Max((incheck) ? MISc : MISn, b->search_dif * (1+0/(7*changes))));
-								b->search_dif = Min(1000, Max((incheck) ? MISn : MISn, b->search_dif + 120/changes));
+								if(changes>=1) b->search_dif = Min(1000, Max((incheck) ? MISn : MISn, b->search_dif + 110/changes));
+//								else b->search_dif +=50;
 							}
+
 							copyTree(tree, ply);
 							tree->tree[ply][ply].score =
 								best;
@@ -1736,17 +1738,17 @@ int IterativeSearchN(board *b, int alfa, int beta, int depth, int side, int star
 // was not stopped during last iteration 
 
 	if(((changes != 1) || (tree->tree[ply][ply].move != b->p_pv.line[0].move)
-		|| ((best+3000) < old_score)) && (f > (start_depth + 1))) {
+		|| ((best+500) < old_score)) && (f > (start_depth + 1))) {
 				b->search_dif =
 					Min(1000,
 						Max((incheck) ? MISn : MISn, b->search_dif*1.0)
 //						+(changes>3)*60
-						+((best+2500) < old_score)*(old_score-best)/40
+						+((best+500) < old_score)*(old_score-best)/50
 //						+(tree->tree[ply][ply].move!= b->p_pv.line[0].move)*30
 //						+100
 						);
 //	} else b->search_dif = Max(0, (best<0) ? b->search_dif -0 : b->search_dif -20) ;
-	} else b->search_dif = Max(0, (best<0) ? b->search_dif -0 : b->search_dif * 0.90) ;
+	} else b->search_dif = Max(0, (best<0) ? b->search_dif -0 : b->search_dif * 0.9) ;
 
 		b->stats->iterations++;
 // clear qorder for moves not processed
